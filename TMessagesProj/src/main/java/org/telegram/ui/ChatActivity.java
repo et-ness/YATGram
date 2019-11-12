aa45eb8c3fbeeaa92f4b64be88e62b6f5b825912
23rd <23rd@vivaldi.net>
2019-11-13 00:43:48 +0300

    Added ability to hide bottom button.

--- TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/ChatActivity.java
@@ -7245,6 +7245,9 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
             if (getParentActivity() == null || pullingDownOffset != 0) {
                 return;
             }
+            if (MessagesController.getGlobalMainSettings().getBoolean("hideBottomButton", false)) {
+                return;
+            }
             if (reportType >= 0) {
                 showDialog(new ReportAlert(getParentActivity(), reportType, getResourceProvider()) {
                     @Override
@@ -22086,6 +22089,9 @@ public class ChatActivity extends BaseFragment implements NotificationCenter.Not
             } else {
                 bottomOverlayChatText.setText(LocaleController.getString("DeleteThisChat", R.string.DeleteThisChat));
             }
+        }
+        if (MessagesController.getGlobalMainSettings().getBoolean("hideBottomButton", false)) {
+            bottomOverlayChatText.setText("");
         }
 
         if (currentChat != null && currentChat.gigagroup && reportType < 0 && chatMode == 0) {
