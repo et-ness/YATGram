c50b4df0442a14a5ca61f32e46cc7c95b9db2876
23rd <23rd@vivaldi.net>
2021-12-29 05:05:45 +0300

    Added ability to set custom title of main view.

--- TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/DialogsActivity.java
@@ -2257,6 +2257,7 @@ public class DialogsActivity extends BaseFragment implements NotificationCenter.
                 } else {
                     actionBar.setTitle(LocaleController.getString("AppName", R.string.AppName));
                 }
+                actionBar.setTitle(MessagesController.getGlobalMainSettings().getString("forkCustomTitle", "Fork Client"));
             }
             if (folderId == 0) {
                 actionBar.setSupportsHolidayImage(true);
