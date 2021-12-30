31d9aa86268909b41fac4c327ca71255a9263462
23rd <23rd@vivaldi.net>
2021-12-30 20:18:02 +0300

    Replaced Google translate with DuckDuckGo.

--- TMessagesProj/src/main/java/org/telegram/messenger/LanguageDetector.java
+++ TMessagesProj/src/main/java/org/telegram/messenger/LanguageDetector.java
@@ -13,45 +13,6 @@ public class LanguageDetector {
     }
 
     public static void detectLanguage(String text, StringCallback onSuccess, ExceptionCallback onFail) {
-        detectLanguage(text, onSuccess, onFail, false);
-    }
-
-    public static void detectLanguage(String text, StringCallback onSuccess, ExceptionCallback onFail, boolean initializeFirst) {
-        try {
-            if (initializeFirst) {
-                com.google.mlkit.common.sdkinternal.MlKitContext.zza(ApplicationLoader.applicationContext);
-            }
-            com.google.mlkit.nl.languageid.LanguageIdentification.getClient()
-                .identifyLanguage(text)
-                .addOnSuccessListener(str -> {
-                    if (onSuccess != null) {
-                        onSuccess.run(str);
-                    }
-                })
-                .addOnFailureListener(e -> {
-                    if (onFail != null) {
-                        onFail.run(e);
-                    }
-                });
-        } catch (IllegalStateException e) {
-            if (!initializeFirst) {
-                detectLanguage(text, onSuccess, onFail, true);
-            } else {
-                if (onFail != null) {
-                    onFail.run(e);
-                }
-                FileLog.e(e, false);
-            }
-        } catch (Exception e) {
-            if (onFail != null) {
-                onFail.run(e);
-            }
-            FileLog.e(e);
-        } catch (Throwable t) {
-            if (onFail != null) {
-                onFail.run(null);
-            }
-            FileLog.e(t, false);
-        }
+        onSuccess.run("Auto");
     }
 }
